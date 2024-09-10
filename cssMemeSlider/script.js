81f407c53d5c27dfd637f10a73b1dffdc4fe5122
Ksenii <kseniimarkova3@gmail.com>
2024-09-10 19:46:14 +0200

    feat: add animation for slider (Tue, Sep 10, 2024 07:46:02 PM)

--- cssMemeSlider/script.js
+++ cssMemeSlider/script.js
@@ -1,5 +1,5 @@
 const slides = document.querySelectorAll('.mem-image');
-const controls = document.querySelectorAll('.control');
+const controls = document.querySelectorAll('.control-container');
 const captions = document.querySelectorAll('.caption');
 
 let currentSlide = 0;
@@ -9,30 +9,34 @@ controls.forEach((control, index) => {
         if (index === currentSlide) return;
 
         // Определение направления анимации
-        const direction = index > currentSlide ? 'slideOutLeft' : 'slideInRight';
+        const oldSlideDirection = index > currentSlide ? 'slideOutLeft' : 'slideOutRight';
+        const newSlideDirection = index > currentSlide ? 'slideIn' : 'slideInRight';
 
         // Смена активного контрола
-        document.querySelector('.control.active').classList.remove('active');
-        control.classList.add('active');
+        const oldControl = document.querySelector('.control-container.activate');
+        oldControl.classList.remove('activate');
+        oldControl.firstElementChild.classList.remove('activate');
+        control.classList.add('activate');  
+        control.firstElementChild.classList.add('activate');
 
         // Смена слайда
         const oldMeme = document.querySelector('.mem-image.active');
         oldMeme.classList.remove('active');
-        oldMeme.style.animationName = direction;
-        console.log(oldMeme.style.animationName = direction);
+        oldMeme.style.animationName = oldSlideDirection;
         oldMeme.classList.add('noactive');
         slides[index].classList.add('active');
+        slides[index].style.animationName = newSlideDirection;
         slides[index].classList.remove('noactive');
 
         //Смена текста
         const oldHeader = document.querySelector('.caption.active');
         oldHeader.classList.remove('active');
-        oldHeader.style.animationName = direction;
+        oldHeader.style.animationName = oldSlideDirection;
         oldHeader.classList.add('noactive');
         captions[index].classList.add('active');
+        captions[index].style.animationName = newSlideDirection;
         captions[index].classList.remove('noactive');
 
         currentSlide = index;
     });
 });
-
